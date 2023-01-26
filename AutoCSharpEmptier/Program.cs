a96fa70c5363b1f72cf0eb8f1606d6620dd34456
Lukas <81436826+L1mee@users.noreply.github.com>
2023-01-26 16:31:58 +0100

    Progressssss but not really

--- AutoCSharpEmptier/Program.cs
+++ AutoCSharpEmptier/Program.cs
@@ -1,21 +1,6 @@
-﻿using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-// ReSharper disable SuggestVarOrType_SimpleTypes
-// ReSharper disable SuggestVarOrType_BuiltInTypes
+﻿using AutoCSharpEmptier;
 
-const string path = @"";
-const string outPath = @"";
-string[] files = Directory.GetFiles(path, "*.cs", SearchOption.AllDirectories);
+const string path = @"C:\Users\Lukas\Documents\Unity\PlugInsTest\EmptyOdin\Assets\Plugins\Sirenix\Odin Inspector\Source";
+const string outPath = @"C:\Users\Lukas\Desktop\NewSource";
 
-foreach (string fileName in files)
-{
-    string input = File.ReadAllText(fileName);
-
-    SyntaxTree tree = CSharpSyntaxTree.ParseText(input);
-    SyntaxNode root = tree.GetRoot();
-    SyntaxNode? newRoot = root.RemoveNodes(root.DescendantNodes().Where(c => c is not AttributeSyntax), SyntaxRemoveOptions.KeepNoTrivia);
-    string modifiedScript = newRoot!.ToFullString();
-    string outputPath = fileName.Replace(path, outPath);
-    File.WriteAllText(outputPath, modifiedScript);
-}
+Methods.RemoveNodesMethod(path, outPath);
